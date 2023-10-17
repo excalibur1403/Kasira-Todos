8dacc1292829217802e33f66e23860a4e64e4585
excalibur1403 <148179106+excalibur1403@users.noreply.github.com>
2023-10-17 11:38:40 +0700

    Update account.py

--- account.py
+++ account.py
@@ -4,7 +4,7 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 from firebase_admin import auth
 
-cred = credentials.Certificate("todo-ecdb5-157ee2bc08d8.json")
+cred = credentials.Certificate("godd-95f95-7055b79444ad.json")
 firebase_admin.initialize_app(cred)
 
 def app():
